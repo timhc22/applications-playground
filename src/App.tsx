9e3551509e5b4518a0e1985eb38cf4a5a47df015
Timothy Coleman <timhc22@gmail.com>
2020-08-05 14:06:07 +0100

    Change to TypeScript and SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <div>
