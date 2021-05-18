ce7a14b0c68dc3d317ad4eb54ff2a387e11956aa
Timothy Coleman <timhc22@gmail.com>
2021-05-18 12:57:20 +0100

    Remove unused

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -12,12 +12,6 @@ export default function DashboardPage(): JSX.Element {
           <Link to="/client/review-applicants" className="button">
             Review Applicants Feed Simulator
           </Link>
-
-          <Divider orientation="horizontal" flexItem />
-          <h2>Candidate</h2>
-          <Link to="/candidate/job-feed" className="button">
-            Job Feed Simulator (in progress)
-          </Link>
         </section>
       </div>
     </section>
